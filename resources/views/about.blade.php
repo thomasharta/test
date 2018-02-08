7beecb9b7df721731814165c995fdb57c5b65035
thomasharta <thomasakb@gmail.com>
2018-02-09 01:13:54 +0700

    First commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>About<h1/>
+	<p>
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	consequat. Duis aute irure dolor in reprehenderit in</p>
+@endsection
---
+++ resources/views/inc/showcase.blade.php
@@ -0,0 +1,6 @@
+<div class="jumbotron text-center">
+	<div class="container">
+		<h1>Selamat Datang!!!</h1>
+		<p class="lead"> i need you girl we o we o saranghae uhuy mantap pisan euy. </p>
+	</div>
+</div>
